684d8d6a09aa7ec7dbd20b882cc941f9a7a3db91
jmcshane <jmcshan1@gmail.com>
2018-01-22 13:39:41 -0600

    Don't need both bridges

--- src/main/java/jenkins/plugins/http_request/HttpRequestBridge.java
+++ src/main/java/jenkins/plugins/http_request/HttpRequestBridge.java
@@ -1,15 +1,8 @@
 package jenkins.plugins.http_request;
 
-import hudson.model.TaskListener;
 import org.jenkinsci.plugins.servicenow.ServiceNowPluginException;
 
 public class HttpRequestBridge {
-    static HttpRequestExecution from(HttpRequestStep step,
-                                     TaskListener taskListener,
-                                     HttpRequestStep.Execution execution) {
-        return HttpRequestExecution.from(step, taskListener, execution);
-    }
-
     public static Object call(HttpRequestStep.Execution httpRequestExecution) {
         try {
             return httpRequestExecution.run();
